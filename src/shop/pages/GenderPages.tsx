416cc346e99c76352f6371c7672da62069d7d730
Claudio Olivares <ca.olivares91@gmail.com>
2026-05-13 20:37:55 -0400

    feat: avance maquetacion

--- src/shop/pages/GenderPages.tsx
+++ src/shop/pages/GenderPages.tsx
@@ -1,5 +1,20 @@
+import { CustomPaginator } from "@/components/custom/CustomPaginator";
+import { products } from "@/mocks/products.mock";
 import React from "react";
+import { CustomJumbotron } from "../components/CustomJumbotron";
+import { ProductsGrid } from "../components/ProductsGrid";
+import { useParams } from "react-router";
 
 export const GenderPages = () => {
-  return <div>GenderPages</div>;
+  const { gender } = useParams();
+
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
+  return (
+    <>
+      <CustomJumbotron title={`Productos para ${genderLabel}`} />
+      <ProductsGrid products={products} />
+      <CustomPaginator totalPages={5} />
+    </>
+  );
 };
